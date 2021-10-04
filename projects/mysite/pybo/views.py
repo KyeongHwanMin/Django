d35e13391324369f484f5f7fe03d764a32eb3fc5
KyeongHwanMin <h45652@naver.com>
2021-10-04 16:28:11 +0900

    refactor(views.py) : 질문 목록 출력

--- projects/mysite/pybo/views.py
+++ projects/mysite/pybo/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
-
-# Create your views here.
-from django.http import HttpResponse
+from .models import Question
 
 def index(request):
-    return HttpResponse("안녕하세요. pybo에 온걸 환여합니다")
+    """
+    pybo 목록 출력
+    """
+    question_list = Question.objects.order_by('-create_date')
+    context = {'question_list' : question_list}
+    return render(request,'pybo/question_list.html', context)
