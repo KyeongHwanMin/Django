8a6be528b7442f6775a18b4043b25f7e3e4108a5
KyeongHwanMin <h45652@naver.com>
2021-10-05 16:49:38 +0900

    refactor(pybo,templates) : views.py 분리 첫번째 방법

--- pybo/urls.py
+++ pybo/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from .views import base_views, question_views, answer_views, comment_views, vote_views
 
 from . import views
 
@@ -19,6 +20,8 @@ urlpatterns = [
     path('comment/create/answer/<int:answer_id>/', views.comment_create_answer, name='comment_create_answer'),
     path('comment/modify/answer/<int:comment_id>/', views.comment_modify_answer, name='comment_modify_answer'),
     path('comment/delete/answer/<int:comment_id>/', views.comment_delete_answer, name='comment_delete_answer'),
-
+    # vote_views.py
+    path('vote/question/<int:question_id>/', vote_views.vote_question, name='vote_question'),
+path('vote/answer/<int:answer_id>/', vote_views.vote_answer, name='vote_answer'),
 
 ]
