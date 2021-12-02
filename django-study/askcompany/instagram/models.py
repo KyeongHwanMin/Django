8bfe6ba9f4992e55711421bcdf7423a156903b03
KyeongHwanMin <h45652@naver.com>
2021-12-02 17:44:00 +0900

    feat : Queryset의 정렬 및 범위 조건

--- django-study/askcompany/instagram/models.py
+++ django-study/askcompany/instagram/models.py
@@ -13,6 +13,9 @@ class Post(models.Model):
         #return f"Custom Post object ({self.id})"
         return self.message
 
+    class Meta:
+        ordering = ['-id']
+
     # def message_length(self):
     #     return len(self.message)
     # message_length.short_description = "메세지 글자수"
