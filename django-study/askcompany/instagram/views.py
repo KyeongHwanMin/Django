28d7ea4b2a7a0579f9712e3b3f80adfb813a5364
KyeongHwanMin <h45652@naver.com>
2021-12-07 16:03:57 +0900

    feat:CBV API (Generic display views)

--- django-study/askcompany/instagram/views.py
+++ django-study/askcompany/instagram/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse, HttpRequest, Http404
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from django.shortcuts import render, get_object_or_404
 from .models import Post
 
@@ -17,13 +17,28 @@ post_list = ListView.as_view(model=Post)
 #     })
 
 
-def post_detail(request: HttpRequest, pk:int) -> HttpResponse:
-    post = get_object_or_404(Post, pk=pk)
-    # try:
-    #     post = Post.objects.get(pk=pk)
-    # except Post.DoesNotExist:
-    #     raise Http404
-    return render(request, 'instagram/post_detail.html',{
-        'post': post,
-    })
+# def post_detail(request: HttpRequest, pk:int) -> HttpResponse:
+#     post = get_object_or_404(Post, pk=pk)
+#     # try:
+#     #     post = Post.objects.get(pk=pk)
+#     # except Post.DoesNotExist:
+#     #     raise Http404
+#     return render(request, 'instagram/post_detail.html',{
+#         'post': post,
+#     })
 
+# post_detail = DetailView.as_view(
+#     model=Post,
+#     queryset=Post.objects.frilter(is_public=True))
+
+class PostDetailView(DetailView):
+    model = Post
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        if not self.request.user.is_authenciated:
+            qs = qs.filter()
+        return qs
+
+
+post_detail = PostDetailView.as_view()
